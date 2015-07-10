69e0fbf6fbec2ad77d4830c4019067f554c850fd
Brett Spencer <bspencer411@gmail.com>
2015-07-09 18:18:20 -0700

    adds news model and controller

---
+++ sql/news-07092015.sql
@@ -0,0 +1,15 @@
+/*
+news-07092015.sql
+used for CI tutorial
+*/
+
+drop table if exist news;
+
+CREATE TABLE news (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        title varchar(128) NOT NULL,
+        slug varchar(128) NOT NULL,
+        text text NOT NULL,
+        PRIMARY KEY (id),
+        KEY slug (slug)
+);
